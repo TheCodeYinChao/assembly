97ce91520f5ee3f87ecefb16e9aa3ecd5ef0f314
zhangyinchao <292306404@qq.com>
2019-04-07 21:57:38 +0800

    完善功能

---
+++ assembly-nio/src/main/java/com/netty/AssemblyNioApplication.java
@@ -0,0 +1,13 @@
+package com.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyNioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyNioApplication.class, args);
+	}
+
+}
