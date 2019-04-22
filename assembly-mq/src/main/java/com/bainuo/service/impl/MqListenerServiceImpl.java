825bb7e68259d33286e57012108010fd2710201b
TheCodeYinChao <292306404@qq.com>
2019-04-22 18:43:59 +0800

    添加 rabbitmq 相关使用的典范

--- assembly-mq/src/main/java/com/bainuo/service/impl/MqListenerServiceImpl.java
+++ assembly-mq/src/main/java/com/bainuo/service/impl/MqListenerServiceImpl.java
@@ -1,12 +1,14 @@
 package com.bainuo.service.impl;
 
 import com.bainuo.service.MqListenerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MqListenerServiceImpl implements MqListenerService {
     @Override
     public void data(String msg) {
-
+        log.info("recevice msg {}",msg);
     }
 }
