e7d1d05fd2d9fe5c919c47f613d44614456c8b19
TheCodeYinChao <292306404@qq.com>
2019-04-22 11:35:02 +0800

    修改模块名称

---
+++ assembly-mq/src/main/java/com/bainuo/AssemblyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.bainuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyKafkaApplication.class, args);
+	}
+
+}
