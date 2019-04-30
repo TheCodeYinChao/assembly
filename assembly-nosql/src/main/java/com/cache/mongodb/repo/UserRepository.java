feb5fb5215e8256d5de16a588c60a6e102541d69
TheCodeYinChao <292306404@qq.com>
2019-04-30 14:03:11 +0800

    加入简单的mongodemo

---
+++ assembly-nosql/src/main/java/com/cache/mongodb/repo/UserRepository.java
@@ -0,0 +1,61 @@
+package com.cache.mongodb.repo;
+
+import com.cache.mongodb.model.User;
+import com.mongodb.client.result.UpdateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * 创建对象
+     * @param user
+     */
+    public void saveUser(User user,String collectName) {
+        mongoTemplate.save(user,collectName);
+    }
+
+    /**
+     * 根据用户名查询对象
+     * @param userName
+     * @return
+     */
+    public User findUserByUserName(String userName) {
+        Query query=new Query(Criteria.where("userName").is(userName));
+        User user =  mongoTemplate.findOne(query , User.class);
+        return user;
+    }
+
+    /**
+     * 更新对象
+     * @param user
+     */
+    public long updateUser(User user) {
+        Query query=new Query(Criteria.where("id").is(user.getId()));
+        Update update= new Update().set("userName", user.getUserName()).set("passWord", user.getPassWord());
+        //更新查询返回结果集的第一条
+        UpdateResult result =mongoTemplate.updateFirst(query,update,User.class);
+        //更新查询返回结果集的所有
+        // mongoTemplate.updateMulti(query,update,UserEntity.class);
+        if(result!=null)
+            return result.getMatchedCount();
+        else
+            return 0;
+    }
+
+    /**
+     * 删除对象
+     * @param id
+     */
+    public void deleteUserById(Long id) {
+        Query query=new Query(Criteria.where("id").is(id));
+        mongoTemplate.remove(query,User.class);
+    }
+}
